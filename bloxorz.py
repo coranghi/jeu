9290f3ab8f822d9d0c06cef8f70c504948c61843
shrek <58484520+coranghi@users.noreply.github.com>
2023-11-07 08:55:04 +0100

    Add files via upload

--- bloxorz.py
+++ bloxorz.py
@@ -1,45 +1,64 @@
-h, w = 10, 10
-p = [[' ']*w for _ in range(h)]
-bx1, bx2, by1, by2 = 0, 1, 0, 0
-cx1, cx2, cy1, cy2 = 9, 9, 9, 8
-p[by1][bx1] = p[by2][bx2] = 'B'
-p[cy1][cx1] = p[cy2][cx2] = 'C'
+# Définitit la taille du plateau
+hauteur, largeur = 10, 10
+plateau = [[' ']*largeur for _ in range(hauteur)]
 
-def s():
-    for l in p: print(' '.join(l))
-    if (bx1 == cx1 and bx2 == cx2 and by1 == cy1 and by2 == cy2) or (bx1 == cx2 and bx2 == cx1 and by1 == cy2 and by2 == cy1): print("Vous avez gagné !"); return True
+# position du bloc et de la cible
+bloc_x1, bloc_x2, bloc_y1, bloc_y2 = 0, 1, 0, 0
+cible_x1, cible_x2, cible_y1, cible_y2 = 9, 9, 9, 8
+plateau[bloc_y1][bloc_x1] = plateau[bloc_y2][bloc_x2] = 'B'
+plateau[cible_y1][cible_x1] = plateau[cible_y2][cible_x2] = 'C'
+
+def afficher_plateau():
+    for ligne in plateau:
+        print(' '.join(ligne))
+        
+def verifier_victoire():
+    if (bloc_x1 == cible_x1 and bloc_x2 == cible_x2 and bloc_y1 == cible_y1 and bloc_y2 == cible_y2) or \
+       (bloc_x1 == cible_x2 and bloc_x2 == cible_x1 and bloc_y1 == cible_y2 and bloc_y2 == cible_y1):
+        print("gagné")
+        return True
+    return False
 
 while True:
-    s()
-    m = input("Déplacez le bloc (g pour gauche, d pour droite, h pour haut, b pour bas, t pour tourner): ").lower()
-    if m == 'g':
-        if bx1 > 0 and bx2 > 0:
-            p[by1][bx1] = p[by2][bx2] = ' '
-            bx1 -= 1
-            bx2 -= 1
-    elif m == 'd':
-        if bx1 < w-1 and bx2 < w-1:
-            p[by1][bx1] = p[by2][bx2] = ' '
-            bx1 += 1
-            bx2 += 1
-    elif m == 'h':
-        if by1 > 0 and by2 > 0:
-            p[by1][bx1] = p[by2][bx2] = ' '
-            by1 -= 1
-            by2 -= 1
-    elif m == 'b':
-        if by1 < h-1 and by2 < h-1 and p[by1+1][bx1] == ' ' and p[by1+1][bx2] == ' ':
-            p[by1][bx1] = p[by2][bx2] = ' '
-            by1 += 1
-            by2 += 1
-    elif m == 't':
-        p[by1][bx1] = p[by2][bx2] = ' '
-        if bx1 == bx2:
-            bx2 += 1
-            by2 -= 1
-        else:
-            bx2 = bx1
-            by2 = by1 + 1
-    p[by1][bx1] = p[by2][bx2] = 'B'
-    if s():
+    afficher_plateau()
+    mouvement = input("Déplace le bloc (g pour gauche, d pour droite, h pour haut, b pour bas, t pour tourner): ").lower()
+    
+    if mouvement == 'g':
+        if bloc_x1 > 0 and bloc_x2 > 0:
+            plateau[bloc_y1][bloc_x1] = plateau[bloc_y2][bloc_x2] = ' '
+            bloc_x1 -= 1
+            bloc_x2 -= 1
+    elif mouvement == 'd':
+        if bloc_x1 < largeur - 1 and bloc_x2 < largeur - 1:
+            plateau[bloc_y1][bloc_x1] = plateau[bloc_y2][bloc_x2] = ' '
+            bloc_x1 += 1
+            bloc_x2 += 1
+    elif mouvement == 'h':
+        if bloc_y1 > 0 and bloc_y2 > 0:
+            plateau[bloc_y1][bloc_x1] = plateau[bloc_y2][bloc_x2] = ' '
+            bloc_y1 -= 1
+            bloc_y2 -= 1
+    elif mouvement == 'b':
+        if bloc_y1 < hauteur - 1 and bloc_y2 < hauteur - 1 and plateau[bloc_y1+1][bloc_x1] == ' ' and plateau[bloc_y2+1][bloc_x2] == ' ':
+            plateau[bloc_y1][bloc_x1] = plateau[bloc_y2][bloc_x2] = ' '
+            bloc_y1 += 1
+            bloc_y2 += 1
+    elif mouvement == 't':
+        if bloc_x1 == bloc_x2:
+            if plateau[bloc_y1+1][bloc_x1] == ' ':
+                plateau[bloc_y1][bloc_x1] = ' '
+                plateau[bloc_y2][bloc_x2] = ' '
+                bloc_x2 += 1
+                bloc_y2 -= 1
+            else:
+                plateau[bloc_y1][bloc_x1] = ' '
+                plateau[bloc_y2][bloc_x2] = ' '
+                bloc_x2 -= 1
+                bloc_y2 += 1
+        plateau[bloc_y1][bloc_x1] = 'B'
+        plateau[bloc_y2][bloc_x2] = 'B'
+        
+    assert 0 <= bloc_x1 < largeur and 0 <= bloc_x2 < largeur and 0 <= bloc_y1 < hauteur and 0 <= bloc_y2 < hauteur, "Le bloc est hors du plateau."
+    
+    if verifier_victoire():
         break
